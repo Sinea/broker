f03b982a7fcf8afe787ce72d5f0ce219b30b6bf0
Tiberiu SINEA <tsinea@bitdefender.com>
2018-08-09 11:31:21 +0300

    Extra checks when unpacking container

--- src/github.com/sinea/network/wire/reder.go
+++ src/github.com/sinea/network/wire/reder.go
@@ -1,6 +1,8 @@
 package wire
 
-import "github.com/sinea/network/io"
+import (
+	"github.com/sinea/network/io"
+)
 
 type wireHandler struct {
 	buffer []byte
@@ -10,17 +12,27 @@ type wireHandler struct {
 func (w *wireHandler) Write(buffer []byte) {
 	w.buffer = append(w.buffer, buffer...)
 
-	if w.buffer[0] != 0xB0 {
-		panic("Invalid header")
-	}
-
-	size := w.buffer[1]
-	if uint8(len(w.buffer)) < size+2 {
+	// We need at least 2 bytes in the buffer (header + size)
+	if len(w.buffer) < 2 {
 		return
 	}
 
-	w.next.Write(w.buffer[2:size+2])
-	w.buffer = w.buffer[:size+2]
+	if w.buffer[0] != 0xB0 {
+		panic("invalid container boundary")
+	}
+
+	size := w.buffer[1]
+	if uint8(len(w.buffer)) < size+3 {
+		return
+	}
+
+	if w.buffer[size+2] != 0x0B {
+		panic("invalid container boundary")
+	}
+
+	payload := w.buffer[2 : size+2]
+	w.buffer = w.buffer[size+3:]
+	w.next.Write(payload)
 }
 
 func NewReader(writer io.Writer) io.Writer {
