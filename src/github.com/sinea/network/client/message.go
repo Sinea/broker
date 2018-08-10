7c2ba1b09b7ca3c4983cdb5074385607c69e7081
Tiberiu SINEA <tsinea@bitdefender.com>
2018-08-10 16:56:33 +0300

    Remove unused code

--- src/github.com/sinea/network/client/message.go
+++ src/github.com/sinea/network/client/message.go
@@ -1,29 +1,5 @@
 package client
 
-type Message interface {
-	Kind() uint8
-	Flags() uint8
-	Body() []byte
-}
-
-type message struct {
-	kind  uint8
-	flags uint8
-	body  []byte
-}
-
-func (m *message) Kind() uint8 {
-	return m.kind
-}
-
-func (m *message) Flags() uint8 {
-	return m.flags
-}
-
-func (m *message) Body() []byte {
-	return m.body
-}
-
 type Hello struct {
 	Name string `json:"name"`
 	Age  uint16 `json:"age"`
