c780c2c7cbc9513e684f8d43e8aa3dd7ffa05987
sinea <bryan.fury.snt@gmail.com>
2019-02-27 17:03:39 +0200

    Consume

--- cmd/broker/main.go
+++ cmd/broker/main.go
@@ -1,13 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Sinea/broker/pkg/broker"
+	"time"
 )
+
+func listen(b broker.Broker, topic string) {
+	for t := range b.Read(topic) {
+		fmt.Println(t)
+	}
+}
 
 func main() {
 	b := broker.New(3)
+	go listen(b, "a")
+	go listen(b, "c")
+	time.Sleep(time.Second)
 	for _, t := range string("abcdefghijklmno1234567890!@#$%^&*()?><:{") {
 		b.Write(string(t), []byte(string(t)))
-
 	}
+
+	time.Sleep(time.Second)
 }
--- pkg/broker/broker.go
+++ pkg/broker/broker.go
@@ -3,6 +3,7 @@ package broker
 import (
 	"fmt"
 	"hash/crc32"
+	"sync"
 )
 
 const MaxShards = 64
@@ -11,16 +12,35 @@ type MessageWriter interface {
 	Write(topic string, data []byte)
 }
 
+type MessageReader interface {
+	Read(topic string) <-chan []byte
+}
+
 type Broker interface {
 	MessageWriter
+	MessageReader
 }
 
 type broker struct {
-	nodeCount uint32 // Total number of nodes in cluster
-	numShards uint32 // Known number of shards
-	shardMask uint64 // Shard bit mask for the current node
+	nodeCount        uint32 // Total number of nodes in cluster
+	topicReplication uint32 // Known number of replicas for each topic
+	shardMask        uint64 // Shard bit mask for the current node
 
 	shards map[uint64]shard
+	topics map[string][]chan []byte
+	lock   *sync.Mutex
+}
+
+func (b *broker) Read(topic string) <-chan []byte {
+	b.lock.Lock()
+	out := make(chan []byte)
+	if _, ok := b.topics[topic]; !ok {
+		b.topics[topic] = make([]chan []byte, 0)
+	}
+	b.topics[topic] = append(b.topics[topic], out)
+	b.lock.Unlock()
+
+	return out
 }
 
 func (b *broker) Write(topic string, data []byte) {
@@ -28,17 +48,24 @@ func (b *broker) Write(topic string, data []byte) {
 	shardID := b.getShardID(topic)
 
 	fmt.Printf("Sending %s to shard %d\n", data, shardID)
+	if out, ok := b.topics[topic]; ok {
+		for _, o := range out {
+			o <- data
+		}
+	}
 }
 
 func (b *broker) getShardID(topic string) uint64 {
 	table := crc32.MakeTable(crc32.IEEE)
 	checksum := crc32.Checksum([]byte(topic), table)
 	// The data should be written to this shard
-	return 1 << ((checksum % b.numShards) % MaxShards)
+	return 1 << ((checksum % b.topicReplication) % MaxShards)
 }
 
 func New(shardCount uint32) Broker {
 	return &broker{
-		numShards: shardCount,
+		lock:             &sync.Mutex{},
+		topicReplication: shardCount,
+		topics:           map[string][]chan []byte{},
 	}
 }
